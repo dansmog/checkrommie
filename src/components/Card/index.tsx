88c7bde57119b60354f104d9ef2f0949b9eb0336
Juwon daniel <juwonanthony@gmail.com>
2022-05-31 18:06:53 +0100

    Add empty image for no apartment images

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,11 +1,19 @@
 import User from "../../assets/images/user";
 import { formatCurrency } from "../../utils/currencyFormatter";
+
+import empty from "../../assets/images/emptystateimage.png";
+
 import "./rommieCard.styles.css";
 
 const RommieCard = ({ data, showDetail }: { data: any; showDetail: any }) => {
   return (
     <div className="rommie__card" onClick={showDetail}>
-      <img src={data?.apartment_medias[0]?.url} alt="" />
+      {data?.apartment_medias.length === 0 ? (
+        <img src={empty} alt="" />
+      ) : (
+        <img src={data?.apartment_medias[0]?.url} alt="" />
+      )}
+
       <div className="card__footer">
         <div className="rommie--details">
           {data?.user?.avatar !== null ? (
